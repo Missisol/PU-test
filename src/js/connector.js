fa12cc53caba5147caecbb76d58bab5a091a4c39
Marina.Shvetskaya <marina.sh@nekidaem.ru>
2021-10-19 15:27:40 +0300

    added card badge

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,6 +3,12 @@ console.log('Hello world!');
 
 window.TrelloPowerUp.initialize({
   'card-badges': function(t, opts) {
-    return [];
+    return t.card('all')
+    .then(function(card) {
+      console.log(card);
+      return [{
+        text: card.idShort,
+      }];
+    })
   }
 });
