7b4afbfc76b394a3aaf00585b9b73e823b4bbcd0
Marina.Shvetskaya <marina.sh@nekidaem.ru>
2021-10-19 17:23:52 +0300

    added board buttons

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,12 +3,10 @@ console.log('Hello world!');
 
 window.TrelloPowerUp.initialize({
   'card-badges': function(t, opts) {
-
     return t.card('all')
     .then(function(card) {
       console.log(card)
       if (!card.coordinates) {
-        // const { latitude, longitude } = card.coordinates;
         return fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=e12699b154eafb2ea213c8050784c38d')
         .then(res => res.json())
         .then(weatherData => {
@@ -22,6 +20,24 @@ window.TrelloPowerUp.initialize({
         }) 
       }
       return [];
+    })
+  },
+
+  'board-buttons': function(t, opts) {
+    return t.board('all')
+    .then(function(board) {
+      console.log(board)
+        return fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=e12699b154eafb2ea213c8050784c38d')
+        .then(res => res.json())
+        .then(weatherData => {
+          console.log(weatherData)
+          return [{
+            text: weatherData.main.temp.toString()
+          }, {
+            text: weatherData.wind.speed.toString()
+          }
+        ]
+        }) 
     })
   }
 });
