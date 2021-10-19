a623305823caaa0d296c25db15ffaa2fed479f4c
Marina.Shvetskaya <marina.sh@nekidaem.ru>
2021-10-19 15:55:37 +0300

    added weather api

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,12 +3,23 @@ console.log('Hello world!');
 
 window.TrelloPowerUp.initialize({
   'card-badges': function(t, opts) {
-    return t.card('all')
+    return t.card('coordinates')
     .then(function(card) {
       console.log(card);
-      return [{
-        text: card.idShort,
-      }];
+      if (card.coordinates) {
+        const { latitude, longitude } = card.coordinates;
+        return fetch('api.openweathermap.org/data/2.5/weather?q=London&appid=e12699b154eafb2ea213c8050784c38d')
+        .then(res => res.json())
+        .then(weatherData => {
+          return [{
+            text: weatherData.main.temp.toString()
+          }, {
+            text: weatherData.wind.speed.toString()
+          }
+        ]
+        }) 
+      }
+      return [];
     })
   }
 });
