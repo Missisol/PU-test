a69752fb16ee5f3e6a10f03d52202b4dfffaf75b
Marina.Shvetskaya <marina.sh@nekidaem.ru>
2021-10-19 16:29:36 +0300

    updated request

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,10 +3,10 @@ console.log('Hello world!');
 
 window.TrelloPowerUp.initialize({
   'card-badges': function(t, opts) {
-    console.log(t.card);
 
-    return t.card('coordinates')
+    return t.card('all')
     .then(function(card) {
+      console.log(card)
       if (!card.coordinates) {
         // const { latitude, longitude } = card.coordinates;
         return fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=e12699b154eafb2ea213c8050784c38d')
